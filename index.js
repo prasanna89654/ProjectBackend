0b2d5ab3587dc61df0cd4e2e5a6106586d51d9d8
prasanna89654 <poudellprasanna789@gmail.com>
2024-09-22 10:54:19 +0545

    chore: fixed minor issues

--- index.js
+++ index.js
@@ -16,11 +16,11 @@ app.get("/", (req, res) => {
     res.send("API is running");
     });
 
-app.use("/", userRoutes);
+app.use("/user", userRoutes);
 
-app.use("/", maintainerRoutes);
+app.use("/maintainer", maintainerRoutes);
 
-app.use("/", complaintRoutes);
+app.use("/complaint", complaintRoutes);
 
 app.use(errorHandler);
 
