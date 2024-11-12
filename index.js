17ec2b8d48ba13b77c8454d091a529041eb6eeaa
prasanna89654 <poudellprasanna789@gmail.com>
2024-11-12 23:01:02 +0545

    changes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express";
-
+import cors from "cors";
 const app = express();
-const PORT = 3000;
+const PORT = 4000;
 import dotenv from "dotenv";
 import userRoutes from "./routes/userRoutes.js";
 import maintainerRoutes from "./routes/maintainerRoutes.js";
@@ -9,7 +9,7 @@ import complaintRoutes from "./routes/complaintRoutes.js";
 import { errorHandler } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
-
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
